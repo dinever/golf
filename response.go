057a6b44cfdbfc158d5ee14c92baaa1fd1a37b4c
dinever <dinever@live.cn>
2015-11-18 20:00:00 -0500

    Use embedded struct for request.go

--- response.go
+++ response.go
@@ -2,15 +2,38 @@ package Yafg
 
 import (
   "net/http"
+  "time"
 )
 
 // A wrapper of http.ResponseWriter
 type Response struct {
-  Response http.ResponseWriter
+  http.ResponseWriter
 }
 
 func NewResponse(res http.ResponseWriter) *Response {
   response := new(Response)
-  response.Response = res
+  response.ResponseWriter = res
   return response
+}
+
+func (res *Response) Send(str string) {
+  res.Write([]byte(str))
+}
+
+func (res *Response) Redirect(url string, code int) {
+  res.Header().Set("Location", url)
+  res.WriteHeader(code)
+}
+
+func (res *Response) SetCookie(key string, value string, expire int) {
+  now := time.Now()
+  expireTime := now.Add(time.Duration(expire) * time.Second)
+  cookie := &http.Cookie{
+    Name: key,
+    Value: value,
+    Path: "/",
+    MaxAge: expire,
+    Expires: expireTime,
+  }
+  http.SetCookie(res, cookie)
 }
