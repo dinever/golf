c323fb64f3d609ce631e0584db52fead8a7b4235
dinever <dinever@live.cn>
2015-11-19 19:53:00 -0500

    Add GetCookie method

--- request.go
+++ request.go
@@ -1,6 +1,7 @@
 package Yafg
 
 import (
+  "strings"
   "net/http"
 )
 
@@ -8,11 +9,21 @@ import (
 type Request struct {
   *http.Request
 	Params map[string]string
+	IP string
 }
 
 func NewRequest(req *http.Request) *Request {
   request := new(Request)
   request.Request = req
+	request.IP = strings.Split(req.RemoteAddr, ":")[0]
   return request
 }
 
+func (req *Request) GetCookie(key string) string {
+  cookie, err := req.Request.Cookie(key)
+  if err != nil {
+    return ""
+  } else {
+    return cookie.Value
+  }
+}
