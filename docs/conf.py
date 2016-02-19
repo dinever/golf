e6d83c0ae1a701f169eb5df56a1fd01bee29a1a3
dinever <dinever@live.cn>
2016-02-19 15:17:37 -0500

    Update documentation.

---
+++ docs/conf.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import time
+sys.path.insert(0, os.path.abspath(".."))
+
+master_doc = "index"
+
+project = "Golf"
+
+version = release = "0.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    ]
+
+primary_domain = 'py'
+default_role = 'py:obj'
+
+autodoc_member_order = "bysource"
+autoclass_content = "both"
+
+# Without this line sphinx includes a copy of object.__init__'s docstring
+# on any class that doesn't define __init__.
+# https://bitbucket.org/birkenfeld/sphinx/issue/1337/autoclass_content-both-uses-object__init__
+autodoc_docstring_signature = False
+
+coverage_skip_undoc_in_source = True
+
+coverage_ignore_functions = [
+    # various modules
+    "doctests",
+    "main",
+
+    # parse_qs_bytes should probably be documented but it's complicated by
+    # having different implementations between py2 and py3.
+    "parse_qs_bytes",
+]
+
+# HACK: sphinx has limited support for substitutions with the |version|
+# variable, but there doesn't appear to be any way to use this in a link
+# target.
+# http://stackoverflow.com/questions/1227037/substitutions-inside-links-in-rest-sphinx
+# The extlink extension can be used to do link substitutions, but it requires a
+# portion of the url to be literally contained in the document.  Therefore,
+# this link must be referenced as :current_tarball:`z`
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+# On RTD we can't import sphinx_rtd_theme, but it will be applied by
+# default anyway.  This block will use the same theme when building locally
+# as on RTD.
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
--- docs/index.rst
+++ docs/index.rst
@@ -4,7 +4,7 @@
 Golf
 ====
 
-`Golf <http://github.com/dinever/golf>`_ is a Go web framework.
+`Golf <http://github.com/dinever/golf>`_ is a fast, simple and lightweight micro-web framework for Go. It comes with powerful features and has no dependencies other than the Go Standard Library.
 
 Installation
 ------------
@@ -22,14 +22,14 @@ Here is a simple "Hello World!" application using Golf::
 
     import "github.com/dinever/golf"
 
-    func helloWorldHandler(req Golf.Request, res Golf.Response) {
-        res.Send("Hello World!")
+    func helloWorldHandler(ctx *Golf.Context) {
+        ctx.Write("Hello World!")
     }
 
     func main() {
         app := Golf.New()
         app.Get("/", helloWorldHandler)
-        app.Run(":5693")
+        app.Run(":9000")
     }
 
 
@@ -39,8 +39,7 @@ Documentation
 .. toctree::
    :titlesonly:
 
-   request
-   response
+   quickstart
 
 LICENSE
 -------
