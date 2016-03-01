4a3a484b607c8831f37fc7176220617cb533e72b
dinever <dinever@live.cn>
2016-02-29 22:58:43 -0500

    Add cookie unittest for Context.

--- context_test.go
+++ context_test.go
@@ -16,13 +16,23 @@ func makeTestHttpRequest(body io.Reader) *http.Request {
 }
 
 func TestContextCreate(t *testing.T) {
-
 	r := makeTestHttpRequest(nil)
 	w := httptest.NewRecorder()
 	app := New()
 	ctx := NewContext(r, w, app)
-
 	if ctx == nil {
 		t.Errorf("Can not create context.")
+	}
+}
+
+func TestCookieSet(t *testing.T) {
+	r := makeTestHttpRequest(nil)
+	w := httptest.NewRecorder()
+	app := New()
+	ctx := NewContext(r, w, app)
+	ctx.SetCookie("foo", "bar", 0)
+	ctx.Send()
+	if w.HeaderMap.Get("Set-Cookie") != `foo=bar; Path=/` {
+		t.Errorf("Cookie test failed: %q != %q", w.HeaderMap.Get("Set-Cookie"), `foo=bar; Path=/`)
 	}
 }
