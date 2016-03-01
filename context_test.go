18270658e4b29fd25ac4547730e00e7d25063b96
dinever <dinever@live.cn>
2016-03-01 16:53:58 -0500

    [test] Added test cases for Context.Query and Context.Redirect.

--- context_test.go
+++ context_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 )
 
-func makeTestHTTPRequest(body io.Reader) *http.Request {
-	req, err := http.NewRequest("GET", "/foo/bar", body)
+func makeTestHTTPRequest(body io.Reader, method, url string) *http.Request {
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil
 	}
@@ -16,7 +16,7 @@ func makeTestHTTPRequest(body io.Reader) *http.Request {
 }
 
 func TestContextCreate(t *testing.T) {
-	r := makeTestHTTPRequest(nil)
+	r := makeTestHTTPRequest(nil, "GET", "/foo/bar/")
 	w := httptest.NewRecorder()
 	app := New()
 	ctx := NewContext(r, w, app)
@@ -26,7 +26,7 @@ func TestContextCreate(t *testing.T) {
 }
 
 func TestCookieSet(t *testing.T) {
-	r := makeTestHTTPRequest(nil)
+	r := makeTestHTTPRequest(nil, "GET", "/foo/bar/")
 	w := httptest.NewRecorder()
 	app := New()
 	ctx := NewContext(r, w, app)
@@ -34,5 +34,47 @@ func TestCookieSet(t *testing.T) {
 	ctx.Send()
 	if w.HeaderMap.Get("Set-Cookie") != `foo=bar; Path=/` {
 		t.Errorf("Cookie test failed: %q != %q", w.HeaderMap.Get("Set-Cookie"), `foo=bar; Path=/`)
+	}
+}
+
+func TestQuery(t *testing.T) {
+	r := makeTestHTTPRequest(nil, "GET", "/search?q=foo&p=bar")
+	w := httptest.NewRecorder()
+	app := New()
+	ctx := NewContext(r, w, app)
+	q, err := ctx.Query("q")
+	if err != nil {
+		t.Errorf("Can not retrieve a query.")
+	} else {
+		if q != "foo" {
+			t.Errorf("Can not retrieve the correct query `q`.")
+		}
+	}
+	p, err := ctx.Query("p")
+	if err != nil {
+		t.Errorf("Can not retrieve a query.")
+	} else {
+		if p != "bar" {
+			t.Errorf("Can not retrieve the correct query `p`.")
+		}
+	}
+}
+
+func makeNewContext(method, url string) *Context {
+	r := makeTestHTTPRequest(nil, method, url)
+	w := httptest.NewRecorder()
+	app := New()
+	return NewContext(r, w, app)
+}
+
+func TestRedirection(t *testing.T) {
+	r := makeTestHTTPRequest(nil, "GET", "/")
+	w := httptest.NewRecorder()
+	app := New()
+	ctx := NewContext(r, w, app)
+	ctx.Redirect("/foo")
+	ctx.Send()
+	if w.HeaderMap.Get("Location") != `/foo` {
+		t.Errorf("Can not perform a 301 redirection.")
 	}
 }
