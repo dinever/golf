4ae356d5687d954980ada01ac7e16f9b3f13ee33
dinever <dinever@live.cn>
2016-04-08 10:52:56 -0400

    [feat] Implemented memory-based session control.

--- session.go
+++ session.go
@@ -1,15 +1,59 @@
 package Golf
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 )
+
+const sessionIDLength = 64
+
+// MemorySessionManager manages a map of sessions.
+type MemorySessionManager struct {
+	sessions map[string]*MemorySession
+}
+
+// NewMemorySessionManager creates a new session manager.
+func NewMemorySessionManager() *MemorySessionManager {
+	mgr := new(MemorySessionManager)
+	mgr.sessions = make(map[string]*MemorySession)
+	return mgr
+}
+
+func (mgr *MemorySessionManager) sessionID() (string, error) {
+	b := make([]byte, sessionIDLength)
+	n, err := rand.Read(b)
+	if n != len(b) || err != nil {
+		return "", fmt.Errorf("Could not successfully read from the system CSPRNG.")
+	}
+	return hex.EncodeToString(b), nil
+}
+
+// Session gets the session instance by indicating a session id.
+func (mgr *MemorySessionManager) Session(sid string) (Session, error) {
+	if s, ok := mgr.sessions[sid]; ok {
+		return s, nil
+	}
+	return nil, fmt.Errorf("Can not retrieve session with id %s.", sid)
+}
+
+// NewSession creates and returns a new session.
+func (mgr *MemorySessionManager) NewSession() (Session, error) {
+	sid, err := mgr.sessionID()
+	if err != nil {
+		return nil, err
+	}
+	s := MemorySession{sid: sid, data: make(map[string]interface{})}
+	mgr.sessions[sid] = &s
+	return &s, nil
+}
 
 // Session is an interface for session instance, a session instance contains
 // data needed.
 type Session interface {
 	Set(key string, value interface{}) error
-	Get(key string) interface{}
-	Delete(key interface{}) error
+	Get(key string) (interface{}, error)
+	Delete(key string) error
 	SessionID() string
 }
 
